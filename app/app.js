fa8e68ac30efd20aeeb62802bbb9fe384b3172e0
Tenebrosful <hugobernard39@gmail.com>
2020-12-02 10:58:00 +0100

    Connexion bd

--- app/app.js
+++ app/app.js
@@ -1,23 +1,23 @@
-const { json } = require('express');
-const express=require('express');
-const app=express();
-const port=process.env.LOCAL_PORT;
+const express = require('express');
+const app = express();
+const port = process.env.LOCAL_PORT;
 
-const MongoClient = require('mongodb').MongoClient;
-const url="mongodb://database";
-const dbName="firstmongodb";
+const MongoClient = require('mongodb').MongoClient
+const url = "mongodb://database";
+const dbName = "firstmongodb";
 
 let db;
-MongoClient.connect(url, function(err, client){
-    if(err)
+MongoClient.connect(url, {useUnifiedTopology: true}, function (err, client) {
+    if (err)
         console.error(err);
-    db=client.db(dbName);
+
+    db = client.db(dbName);
 });
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.status(200).json(null);
 });
-app.get('/test', (req, res)=>{
+app.get('/test', (req, res) => {
     res.status(200).json(null);
 });
 
