69cfa359998dd90794dc33a001251e27d0efa83f
Gardel Valentin <valentin.gardel2@etu.univ-lorraine.fr>
2020-12-02 10:23:52 +0100

    route fix

--- app/app.js
+++ app/app.js
@@ -1,3 +1,8 @@
+const { json } = require('express');
+const express=require('express');
+const app=express();
+const port=process.env.LOCAL_PORT;
+
 const MongoClient = require('mongodb').MongoClient;
 const url="mongodb://database";
 const dbName="firstmongodb";
@@ -8,3 +13,12 @@ MongoClient.connect(url, function(err, client){
         console.error(err);
     db=client.db(dbName);
 });
+
+app.get('/', (req, res)=>{
+    res.status(200).json(null);
+});
+app.get('/test', (req, res)=>{
+    res.status(200).json(null);
+});
+
+app.listen(port);
