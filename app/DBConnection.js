0107f4f16fc2a4a5e66220f9a479fee890e5a783
Tenebrosful <hugobernard39@gmail.com>
2020-12-09 12:21:41 +0100

    Correction type

--- app/DBConnection.js
+++ app/DBConnection.js
@@ -5,4 +5,8 @@ const dbName = process.env.MONGO_INITDB_DATABASE;//"firstmongodb";
 const db = new MongoClient(url + "/" + dbName, {useUnifiedTopology: true});
 db.connect();
 
+/**
+ *
+ * @type {Db}
+ */
 module.exports = db;
