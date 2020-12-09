9e97331c094e77b13ef0dda9ddfe678a415c894d
Gardel Valentin <valentin.gardel2@etu.univ-lorraine.fr>
2020-12-09 10:47:53 +0100

    update retrieveData

--- app/retrieveData.js
+++ app/retrieveData.js
@@ -20,18 +20,35 @@ function sendXhrPromise(url){
     });
 }
 
-function retrieveParkings(data){
+function updateParkings(data){
     let parkings = data.features;
 
     parkings.forEach(parking => {
-        db.collection('ptsInteret').insertOne();
+        let myParking={
+            nom: parking.NOM,
+            adresse: parking.ADRESS,
+            categorie_id:"",
+            caracteristique: {
+                capacite: parking.CAPACITE,
+                places: parking.PLACES
+            },
+            coordonnee: {
+                x: parking.x,
+                y: parking.y
+            }
+        };
+
+        db.collection('ptsInteret').insertOne(myParking, (err, res)=>{
+            if (err) throw err;
+            console.log("1 parking inserted");
+        });
     });
 }
 
-function retrieveData(){
+function retrieveParkings(){
     let urlParking="https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson";
 
-    sendXhrPromise(urlParking).then(retrieveParkings).catch((error)=>{console.error(error)});
+    sendXhrPromise(urlParking).then(updateParkings).catch((error)=>{console.error(error)});
 }
 
-module.exports = retrieveData;
+module.exports = { retrieveParkings };
